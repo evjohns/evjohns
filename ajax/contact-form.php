c31c32949903c18b1647654ccfc42fa6c6229794
evjohns <ev@evjohns.com>
2016-11-28 15:38:14 +0000

    first commit

---
+++ ajax/contact-form.php
@@ -0,0 +1,32 @@
+<?
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = "Thanks for your Message.";
+$messageReceived = $_POST["message"];
+
+$messageSent = "
+<html>
+<head>
+<title>Email from evjohns.com</title>
+</head>
+<body>
+<p>Dear " . $name . ",</p>
+<p></p>
+<p>Thanks for your message on evjohns.com</p>
+<p></p>
+<p>I will reply as soon as possible.</p>
+<p></p>
+<p>Kind regards,</p>
+<p></p>
+<p>Evan</p>
+</body>
+</html> ";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: ev@evjohns.com" . "\r\n";
+$headerReceived = "From: " . $email . "\r\n";
+
+mail("ev@evjohns.com", "New message on evjohns.com" ,$messageReceived, $headerReceived);
+mail($email, $subject, $messageSent, $headers);
